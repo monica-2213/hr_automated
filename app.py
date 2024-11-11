782b547cf307532bf806640d293f9246aec67bfd
EVANGELINE MONICA A/P RAJEN <93172320+monica-2213@users.noreply.github.com>
2024-11-11 16:58:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import string
 from PyPDF2 import PdfReader
 
 
-jamai = JamAI(api_key="YOUR API KEY HERE", project_id="proj_891eb22841ba077090adc207")
+jamai = JamAI(api_key="google-oauth2|107308287155969914570", project_id="proj_891eb22841ba077090adc207")
 
 
 # Function to extract text from PDF
